4941f50375c42535fa087ec225a9b2c2d6af4d15
Morten Brudvik <morten@brudvik.com>
2024-03-21 20:39:33 +0100

    chore: Add cdn import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, signOut, OAuthProvider, onAuthStateChanged  } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut, OAuthProvider, onAuthStateChanged  } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
